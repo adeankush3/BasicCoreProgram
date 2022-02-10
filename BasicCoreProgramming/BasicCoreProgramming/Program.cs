618bb4a9f9f7cf76d5c9373eb01dffe730b54914
adeankush3 <adeankush3@gmail.com>
2022-02-10 19:56:04 +0530

    [Ankush]Basic Core Programming

--- BasicCoreProgramming/BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/BasicCoreProgramming/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace BasicCoreProgramming
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Basic Core Programming");
         }
     }
 }
